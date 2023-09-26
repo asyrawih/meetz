aaf9ff6ea81ed42a13db2e0894a16ee70e9e0ac7
asyrawih <hasyrawi@gmail.com>
2023-09-26 18:15:40 +0800

    add new pages

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,24 @@ export default function Home() {
   return (
     <main>
       <Navbar />
-      <Container className='flex flex-col my-auto mx-auto justify-center max-w-screen-xl items-center'>
-        <JoinRoom className='w-full'>
-        </JoinRoom>
+      <Container className='flex'>
+        <div className='flex flex-col'>
+          <h3 className='text-5xl flex mx-2 my-2'>
+            Rapat Video Premium Sekarang gratis untuk semua orang
+          </h3>
+          <div className='my-3'></div>
+          <h4 className='text-xl mx-2 my-2'>
+            Nikmati Layanan Video Confrance Secara Gratis <br />
+            Dengan Performa Yang Cukup Cepat
+            Letency dibawah 100ms dengan concurent user yang
+            Cukup Banyak Dalam Tiap Room Nya
+          </h4>
+          <div className='mx-2 my-2'>
+            <Button variant={'outline'}>Rapat Instant</Button>
+          </div>
+        </div>
+        <div className='flex flex-col'>
+        </div>
       </Container>
     </main>
   )
