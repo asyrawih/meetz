036f816b569961536da724d02e3755f3cfe1e544
asyrawih <hasyrawi@gmail.com>
2023-09-27 19:57:00 +0800

    fix sidebar

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,19 +6,21 @@ import { CardContent, CardFooter, CardHeader } from "@/components/ui/card";
 export default function Dashboard() {
   return (
     <>
-      <Sidebar>
-        <SidebarHeader className="flex border border-gray-100-black">
-          <div className="flex justify-between">
-            <div className="inline-flex items-center text-2xl">Meetz</div>
-            <ThemeToggle />
-          </div>
-        </SidebarHeader>
-        <SidebarContent>
-          <div>content</div>
-          <div>content</div>
-          <div>content</div>
-          <div>content</div>
-        </SidebarContent>
+      <Sidebar className="flex flex-col justify-between">
+        <div className="sidebar-header-content">
+          <SidebarHeader className="flex border border-gray-100-black">
+            <div className="flex justify-between">
+              <div className="inline-flex items-center text-2xl">Meetz</div>
+              <ThemeToggle />
+            </div>
+          </SidebarHeader>
+          <SidebarContent className="mt-4 mb-2">
+            <div className="mb-4">content</div>
+            <div className="mb-4">content</div>
+            <div className="mb-4">content</div>
+            <div className="mb-4">content</div>
+          </SidebarContent>
+        </div>
         <CardFooter>
           footer
         </CardFooter>
