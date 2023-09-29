ae2f6b6fc01b8c30e1bca109f8a010377c69cd8c
asyrawih <hasyrawi@gmail.com>
2023-09-29 16:42:44 +0800

    improve calendar

--- components/custom/calendar.tsx
+++ components/custom/calendar.tsx
@@ -2,18 +2,39 @@
 
 import FullCalendar from "@fullcalendar/react"
 import Daygrid from "@fullcalendar/daygrid"
-import { useRef } from "react"
-import { CalendarOptions } from "@fullcalendar/core/index.js"
+import Interaction, { DateClickArg } from "@fullcalendar/interaction"
+import { ClassAttributes, useEffect, useRef } from "react"
+import { CalendarOptions, EventClickArg } from "@fullcalendar/core/index.js"
 
 
 export const Calendar = () => {
 
+  const ref = useRef(null)
+
+  const handlerEvent = (arg: DateClickArg) => {
+    alert(arg.dateStr)
+  }
+
+  useEffect(() => {
+
+  },[])
+
   return (
     <FullCalendar
+      ref={ref}
       headerToolbar={{
         start: "today prev next",
         end: "dayGridMonth dayGridWeek dayGridDay",
       }}
-      plugins={[Daygrid]} />
+      events={[
+        { title: "Event 1", date: "2023-09-06" },
+        { title: "Event 2", date: "2023-09-06" },
+        { title: "Event 2", date: "2023-09-29" },
+        { title: "Event 3", date: "2023-09-06", display: "test", color: "red" },
+      ]}
+      dateClick={handlerEvent}
+      plugins={[Daygrid, Interaction]}
+    />
+
   )
 }
