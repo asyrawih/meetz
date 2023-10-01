cfb5b99138dcd32856fe6c1db9ef79941c4b2838
asyrawih <hasyrawi@gmail.com>
2023-10-01 19:38:54 +0800

    remove navbar on join pages

--- app/join/layout.tsx
+++ app/join/layout.tsx
@@ -3,7 +3,6 @@ import Navbar from "@/components/custom/navbar";
 export default function RoomLayout({ children, }: { children: React.ReactNode }) {
   return (
     <section>
-      <Navbar />
       {children}
     </section>
   )
