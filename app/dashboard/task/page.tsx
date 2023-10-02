3234b2f2a2bdaa089b717c8a2acd4036d4edd4c5
asyrawih <hasyrawi@gmail.com>
2023-10-02 13:45:53 +0800

    add create task dialog

--- app/dashboard/task/page.tsx
+++ app/dashboard/task/page.tsx
@@ -1,6 +1,11 @@
+'use client'
 import { KanbanBoard } from "@/components/section/kanban";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger } from "@radix-ui/react-context-menu";
 import React from "react";
 
 export default function TaskPage() {
@@ -10,13 +15,37 @@ export default function TaskPage() {
         <CardHeader>
           <div className="flex justify-between">
             <h1 className="text-2xl font-semibold">Task</h1>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button>Create Task</Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>Create Task</DialogTitle>
+                  <DialogDescription>
+                    create task after creating of task will automatically will placed on todo board section
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="name" className="text-right">
+                      Task
+                    </Label>
+                    <Input id="name" className="col-span-3" />
+                  </div>
+                </div>
+                <DialogFooter>
+                  <Button type="submit">Save changes</Button>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
           </div>
         </CardHeader>
-        <KanbanBoard />
         <CardContent>
+          <KanbanBoard />
         </CardContent>
         <CardFooter>
-          Footer
+
         </CardFooter>
       </Card>
     </div>
