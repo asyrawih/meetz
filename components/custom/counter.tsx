a75dd54f4657314e38d3a63b1be32e36f76f6082
Hadi Yusuf <61743904+yusufhadi@users.noreply.github.com>
2023-10-04 22:17:54 +0800

    fix error vercel (#5)

--- components/custom/counter.tsx
+++ components/custom/counter.tsx
@@ -1,24 +1,18 @@
-'use client'
+"use client";
 
-import { useContext, useEffect, useState } from "react"
-import { Button } from "../ui/button"
-import { AddCounter } from "./AddCounter"
-import { useCounter } from "@/hooks/counter"
-import { useEnsureTrackRef } from "@livekit/components-react"
-import { count } from "console"
-import { TodoContext, TodoContextType } from "@/provider/todo"
-import { MinusCounter } from "./MinusCounter"
+import { TodoContext } from "@/provider/todo";
+import { useContext } from "react";
+import { AddCounter } from "./AddCounter";
 
 export const Counter = () => {
-  const context = useContext(TodoContext)
-  if (!context) return
-  const { todos } = context
+  const context = useContext(TodoContext);
+  if (!context) return;
+  const { todos } = context;
 
   return (
     <div className="flex flex-col text-2xl">
       {JSON.stringify(todos, null, 2)}
       <AddCounter />
     </div>
-  )
-}
-
+  );
+};
