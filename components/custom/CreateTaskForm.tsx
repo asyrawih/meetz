fe0dcd0aab4e09c6630bac69d793c08000c01166
yusufhadi <hadiyusuf889@gmail.com>
2023-10-05 15:00:47 +0800

    Update Field Status CreateTask

--- components/custom/CreateTaskForm.tsx
+++ components/custom/CreateTaskForm.tsx
@@ -1,52 +1,72 @@
-'use client'
+"use client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import { Button } from "../ui/button";
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { FormEventHandler, useState } from "react";
 import { Textarea } from "../ui/textarea";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Todo } from "../section/kanban";
 
-
-const supabase = createClientComponentClient()
+const supabase = createClientComponentClient();
 
 export const CreateTaskForm = () => {
-
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
   const FormSchema = z.object({
     title: z.string().min(2),
-    status: z.enum(['todo', 'progress', 'test', 'done']),
-    description: z.string().min(10)
+    status: z.enum(["todo", "progress", "test", "done"]),
+    description: z.string().min(10),
   });
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
       title: "",
-      status: 'todo',
-      description: ''
-    }
+      status: "todo",
+      description: "",
+    },
   });
 
-
   const handleAddTask = async (e: z.infer<typeof FormSchema>) => {
-    await supabase.from('todos').insert({
+    await supabase.from("todos").insert({
       title: e.title,
       status: e.status,
-      description: e.description
-    })
-    setOpen(false)
-  }
-
+      description: e.description,
+    });
+    setOpen(false);
+  };
 
   return (
-    <Dialog onOpenChange={setOpen} open={open} >
+    <Dialog onOpenChange={setOpen} open={open}>
       <DialogTrigger asChild>
         <Button>Create Task</Button>
       </DialogTrigger>
@@ -54,7 +74,8 @@ export const CreateTaskForm = () => {
         <DialogHeader>
           <DialogTitle>Create Task</DialogTitle>
           <DialogDescription>
-            create task after creating of task will automatically will placed on todo board section
+            create task after creating of task will automatically will placed on
+            todo board section
           </DialogDescription>
         </DialogHeader>
         <Form {...form}>
@@ -64,39 +85,65 @@ export const CreateTaskForm = () => {
               name="title"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel className="font-semibold leading-4 tracking-wider">Title</FormLabel>
+                  <FormLabel className="font-semibold leading-4 tracking-wider">
+                    Title
+                  </FormLabel>
                   <FormControl className="w-full">
-                    <Input placeholder="Title Task" className="col-span-3" {...field} />
+                    <Input
+                      placeholder="Title Task"
+                      className="col-span-3"
+                      {...field}
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
-              )} />
+              )}
+            />
 
             <FormField
               control={form.control}
               name="status"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel className="font-semibold leading-4 tracking-wider">Status</FormLabel>
+                  <FormLabel className="font-semibold leading-4 tracking-wider">
+                    Status
+                  </FormLabel>
                   <FormControl className="w-full">
-                    <Input placeholder="Status" className="col-span-3" {...field} />
+                    <Select>
+                      <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Select a Status" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectLabel>Status</SelectLabel>
+                          <SelectItem value="todo">Todo</SelectItem>
+                          <SelectItem value="progress">Progress</SelectItem>
+                          <SelectItem value="test">Test</SelectItem>
+                          <SelectItem value="done">Done</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
                   </FormControl>
                   <FormMessage />
                 </FormItem>
-              )} />
+              )}
+            />
 
             <FormField
               control={form.control}
               name="description"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel className="font-semibold leading-4 tracking-wider">Status</FormLabel>
+                  <FormLabel className="font-semibold leading-4 tracking-wider">
+                    Deskripsi
+                  </FormLabel>
                   <FormControl className="w-full">
                     <Textarea placeholder="description" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
-              )} />
+              )}
+            />
 
             <DialogFooter className="mt-4">
               <Button type="submit">Save changes</Button>
@@ -107,4 +154,3 @@ export const CreateTaskForm = () => {
     </Dialog>
   );
 };
-
