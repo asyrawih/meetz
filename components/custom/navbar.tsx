9009f8f035a62d7104167f73839aa3eb6bb4e282
Hadi Yusuf <61743904+yusufhadi@users.noreply.github.com>
2023-10-15 11:54:56 +0800

    Update component navbar and sidebar (#8)
    * fix error vercel
    
    * Update Field Status CreateTask
    
    * fixing bug send value
    
    * update ui component sidebar and navbar
    
    * optimize code
    
    * fix conflict
    
    ---------
    
    Co-authored-by: asyrawi <hasyrawi@gmail.com>

--- components/custom/navbar.tsx
+++ components/custom/navbar.tsx
@@ -1,59 +1,85 @@
-'use client'
-import { useTheme } from "next-themes"
+"use client";
+import { useTheme } from "next-themes";
 
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
+
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
+  User,
+  createClientComponentClient,
+} from "@supabase/auth-helpers-nextjs";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { useEffect, useMemo, useState } from "react";
 
-import { Button } from "../ui/button"
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
-import Link from "next/link"
+const menuItem = ["Home", "About", "Projects"];
 
-const menuItem = [
-  "Home",
-  "About",
-  "Projects"
-]
+const LoginUserState = ({ user }: { user?: User }) => {
+  return (
+    <>
+      <li className="mx-2 cursor-pointer">
+        <Link href={"/dashboard"}>Dashboard</Link>
+      </li>
+      <li className="mx-2 font-semibold">
+        Hello, {user != null ? user.email : "loading..."}
+      </li>
+    </>
+  );
+};
 
 export default function Navbar() {
+  const [dataUser, setDataUser] = useState<User | null>(null);
+  const user = useMemo(() => dataUser, [dataUser]);
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const supabase = createClientComponentClient();
+      try {
+        const {
+          data: { session },
+        } = await supabase.auth.getSession();
+        if (session) {
+          setDataUser(session.user);
+        }
+      } catch (error) {
+        console.error("error fetch data user : ", error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
 
   return (
     <nav className="bg-white border-gray-200 dark:bg-card dark:border-gray-700">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" className="flex items-center">
-          <span className="self-center text-4xl font-semibold whitespace-nowrap dark:text-white">Meetz</span>
+          <span className="self-center text-4xl font-semibold whitespace-nowrap dark:text-white">
+            Meetz
+          </span>
         </a>
         <div className="hidden w-full md:block md:w-auto">
           <ul className="inline-flex mx-3">
             <li className="mx-2 cursor-pointer">Projects</li>
             <li className="mx-2 cursor-pointer">About</li>
-            <li className="mx-2 cursor-pointer">
-              <Link href={'/auth'}>Login</Link>
-            </li>
+            {user !== null ? (
+              <LoginUserState user={user} />
+            ) : (
+              <span>Loading..</span>
+            )}
           </ul>
           <ThemeToggle />
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
-
 const ThemeToggle = () => {
-  const { setTheme } = useTheme()
+  const { setTheme } = useTheme();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -72,5 +98,5 @@ const ThemeToggle = () => {
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
-}
+  );
+};
